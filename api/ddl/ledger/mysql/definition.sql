5703c9a1012af7b983fd9c7fd779030184ba2737
Gaffer Fitch <gaffer.fitch@gmail.com>
2025-02-17 19:12:34 -0500

    Has full blown entity now.

--- api/ddl/ledger/mysql/definition.sql
+++ api/ddl/ledger/mysql/definition.sql
@@ -1,19 +1,46 @@
+CREATE TABLE IF NOT EXISTS `ledger`.`entity` (
+  `id` BIGINT AUTO_INCREMENT,
+  `unum_id` BIGINT,
+  `who` VARCHAR(255) NOT NULL,
+  `meta` JSON NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `unum_id_who` (`unum_id`,`who`)
+);
+
 CREATE TABLE IF NOT EXISTS `ledger`.`fact` (
   `id` BIGINT AUTO_INCREMENT,
-  `origin_id` BIGINT,
+  `witness_id` BIGINT,
   `who` VARCHAR(255) NOT NULL,
   `when` BIGINT,
   `what` JSON NOT NULL,
+  `meta` JSON NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `when` (`when`),
-  UNIQUE `origin_id_who` (`origin_id`,`who`)
+  UNIQUE `witness_id_who` (`witness_id`,`who`)
 );
 
 CREATE TABLE IF NOT EXISTS `ledger`.`origin` (
   `id` BIGINT AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(255),
+  `who` VARCHAR(255) NOT NULL,
   `meta` JSON NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE `name` (`name`)
+  UNIQUE `who` (`who`)
+);
+
+CREATE TABLE IF NOT EXISTS `ledger`.`unum` (
+  `id` BIGINT AUTO_INCREMENT,
+  `who` VARCHAR(255) NOT NULL,
+  `meta` JSON NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `who` (`who`)
+);
+
+CREATE TABLE IF NOT EXISTS `ledger`.`witness` (
+  `id` BIGINT AUTO_INCREMENT,
+  `entity_id` BIGINT,
+  `origin_id` BIGINT,
+  `who` VARCHAR(255) NOT NULL,
+  `meta` JSON NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `entity_id_origin_id_who` (`entity_id`,`origin_id`,`who`)
 );
